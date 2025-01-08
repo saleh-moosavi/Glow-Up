5b1e5aa013d0b19d55e6b858b3e1a0cd984f5bd0
Saleh Moosavi <salehmoosavi77@gmail.com>
2025-01-08 16:40:02 +0330

    fix : Stars section works right when hover happen

--- src/components/Star.tsx
+++ src/components/Star.tsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
+const stars = [0, 1, 2, 3, 4];
+
 export default function Star() {
+  const [starIndex, setStarIndex] = useState<null | number>(null);
   return (
-    <ul className="flex gap-x-[0.15rem] hover:*:text-emerald-700 *:text-xs mt-2 *:transition-all *:duration-300 *:cursor-pointer">
-      <FaStar />
-      <FaStar />
-      <FaStar />
-      <FaStar />
-      <FaStar />
+    <ul className="flex gap-x-[0.15rem] *:text-sm mt-2 *:transition-all *:duration-300 *:cursor-pointer">
+      {stars.map((star) => (
+        <FaStar
+          key={star}
+          className={`${
+            starIndex !== null && starIndex >= star && "text-emerald-700"
+          }`}
+          onMouseEnter={() => setStarIndex(star)}
+          onMouseLeave={() => setStarIndex(null)}
+        />
+      ))}
     </ul>
   );
 }
